7c3f878e68e6139fc3745827aa2f5759e052faa6
devfel <devfelizardo@gmail.com>
2022-10-29 15:26:20 -0400

    logs to check error on rper.members

--- src/modules/rpers/infra/http/middlewares/ensureRperMember.ts
+++ src/modules/rpers/infra/http/middlewares/ensureRperMember.ts
@@ -20,12 +20,27 @@ export async function ensureRperMember(
         throw new AppError('RPER not found', 404);
     }
 
+    console.log("--- COORDINATOR ---");
     console.log(rper.coordinator_id !== user_id);
-    console.log(!!rper.members?.find(rper => rper.user_id === user_id));
+    console.log("RPER Coordinator ID: " + rper.coordinator_id);
+    console.log("User ID: " + user_id);
 
+
+    console.log("--- MEMBERS ---");
+    console.log(!!rper.members?.find(rper => rper.user_id === user_id))
+    console.log("RPER ID: " + rper.rper_id)
+    console.log("RPER Members:" + rper.members);
+    rper.members?.find(rper => {
+        console.log("RPER User_ID: " + rper.user_id);
+        console.log("User ID: " + user_id);
+        return rper.user_id === user_id;
+    })
+
+
+    // both bellow has to be true to throw the error. //Meaning the user logged in is neither a coordinator nor a member. 
     if (
-        rper.coordinator_id !== user_id ||
-        !!rper.members?.find(rper => rper.user_id === user_id)
+        rper.coordinator_id !== user_id &&                      // if the coordinator ID is different of the user logged in
+        !rper.members?.find(rper => rper.user_id === user_id)   // Negate if find the user logged in ID in the members list
     ) {
         throw new AppError('User does not have permission. User is not a Member nor the Coordinator of this RPER.', 403);
     }
