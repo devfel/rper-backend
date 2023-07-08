4e9e2833f34b5351e027f5c9f1846b733364317a
Raul Neto <raulneto90@gmail.com>
2023-07-08 15:56:21 -0300

    feat: implemented get status by section

--- src/modules/rpers/services/GetRperSectionStatusService.ts
+++ src/modules/rpers/services/GetRperSectionStatusService.ts
@@ -1,11 +1,12 @@
-import { inject, injectable } from 'tsyringe';
-import { IRpersSecondaryDataRepository } from '../repositories/IRpersSecondaryDataRepository';
-import AppError from '@shared/errors/AppError';
-import { RperSection } from 'enums';
+import { inject, injectable } from 'tsyringe'
+import { IRpersSecondaryDataRepository } from '../repositories/IRpersSecondaryDataRepository'
+import AppError from '@shared/errors/AppError'
+import { RperSection } from 'enums'
+import { IRperAcknowledgmentRepository } from '../repositories/IRperAcknowledgmentRepository'
 
 interface ExecuteParams {
-  rper_id: string;
-  section: string;
+  rper_id: string
+  section: string
 }
 
 @injectable()
@@ -13,17 +14,30 @@ export class GetRperSectionStatusService {
   constructor(
     @inject('RpersSecondaryDataRepository')
     private readonly rpersSecondaryDataRepository: IRpersSecondaryDataRepository,
+
+    @inject('RpersAcknowledgmentRepository')
+    private readonly rperAcknowledgmentRepository: IRperAcknowledgmentRepository,
   ) {}
 
   async execute({ rper_id, section }: ExecuteParams) {
     if (section === RperSection.SECONDARY_DATA) {
-      const rper = await this.rpersSecondaryDataRepository.findByRperId(rper_id);
+      const rper = await this.rpersSecondaryDataRepository.findByRperId(rper_id)
 
       if (!rper) {
-        throw new AppError('RPER not found', 404);
+        throw new AppError('RPER not found', 404)
       }
 
-      return { status: rper.status };
+      return { status: rper.status }
+    }
+
+    if (section === RperSection.ACKNOWLEDGMENT) {
+      const rper = await this.rperAcknowledgmentRepository.findByRperId(rper_id)
+
+      if (!rper) {
+        throw new AppError('RPER not found', 404)
+      }
+
+      return { status: rper.status }
     }
   }
 }
